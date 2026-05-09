a2ded8b34b8c5c2e0adf6b9f4a7a764e0d2e984a
MARCO <jmukhammadsodikh@gmail.com>
2026-05-09 23:01:15 +0900

    fix: modify project settings to serve static folder

--- madagascar/settings.py
+++ madagascar/settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+print("BASE_DIR:", BASE_DIR)
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ static/js/test.js
@@ -0,0 +1,1 @@
+console.log("This is created for test")
