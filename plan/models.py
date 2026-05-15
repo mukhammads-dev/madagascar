028ba4d307bff3382632175280dce1c4fe3db45d
MARCO <jmukhammadsodikh@gmail.com>
2026-05-15 13:42:21 +0900

    feat: CRUD: practise Read logic

--- plan/models.py
+++ plan/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.db import models, connection
 
 # Plan model
+
+
+class Plan():
+    # state
+
+    # constructor
+    def __init__(self):
+        self.connection = connection
+
+    # method
+    def get_home(self):
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM plan WHERE 1 = 1")
+            result = cursor.fetchall()
+            print(result)
--- plan/views.py
+++ plan/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponseServerError
 from django.shortcuts import render
+from plan.models import Plan
+plan = Plan()
 
 # Plan controller[VIEW]
 
@@ -8,8 +10,9 @@ def get_home(request):
     try:
         print("\n get_home")
         # Plan Model > data
-        data = "MIT"
-        return render(request, "home.html", {'data': data}, status=200)
+        plan.get_home()
+        return render(request, "home.html", {}, status=200)
+
     except Exception as err:
         print("Error in get_home:", err)
         return HttpResponseServerError("Something went wrong")
