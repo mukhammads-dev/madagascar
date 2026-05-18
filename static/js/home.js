1b649afe39d4d87b4861e09e5344fefcbfc75b0e
MARCO <jmukhammadsodikh@gmail.com>
2026-05-18 17:37:38 +0900

    feat: develop update_plan Rest API

--- static/js/home.js
+++ static/js/home.js
@@ -1,8 +1,5 @@
 console.log("%c home.js loaded", "color:blue");
 
-const form_object = document.getElementById("create-form");
-console.log("form_object:", form_object);
-
 function generateTemplate(new_plan) {
     return `  <li
             class="plan list-group-item bg-light d-flex align-items-center justify-content-between custom-list"
@@ -10,14 +7,14 @@ function generateTemplate(new_plan) {
             <span style="color:green" class="item-text">${new_plan.content}</span>
             <div>
               <button
-                data-id="{{ new_plan.id }}"
+                data-id="${new_plan.id}"
                 class="edit-me btn btn-success btn-sm mr-1 custom-radius"
               >
                 <i style="margin-right: 5px" class="bi bi-pencil-square"></i
                 >Edit
               </button>
               <button
-                data-id="{{ new_plan.id }}"
+                data-id="${new_plan.id}"
                 class="delete-me btn btn-danger btn-sm custom-radius"
               >
                 <i style="margin-right: 5px" class="bi bi-trash"></i> Delete
@@ -25,7 +22,7 @@ function generateTemplate(new_plan) {
             </div>
           </li>`;
 }
-
+const form_object = document.getElementById("create-form");
 form_object.addEventListener("submit", function (event) {
     // stop Traditional API
     event.preventDefault();
@@ -33,11 +30,12 @@ form_object.addEventListener("submit", function (event) {
     const input_value = document.getElementById("create-field").value;
 
     console.log("STEP1: Frontend > Rest API REQUEST > Backend");
+    // start Rest API
     axios
         .post("/create_plan", { content: input_value })
         .then((response) => {
             console.log("STEP6: Frontend recieved back API Response");
-            console.log("AXIOS Response:", response);
+            console.log("AXIOS Response Create:", response);
             const { status, result } = response.data;
             const new_plan = {
                 id: result,
@@ -55,4 +53,45 @@ form_object.addEventListener("submit", function (event) {
         .catch((err) => {
             console.log("Creating plan, Error:", err)
         });
+});
+
+
+
+
+document.addEventListener("click", function (e) {
+    console.log("event:", e);
+
+    if (e.target.classList.contains("edit-me")) {
+
+        const user_input = prompt(
+            "Change your plan",
+            e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+        );
+
+        if (user_input) {
+            // start Rest API
+            axios
+                .post("/update_plan", {
+                    id: e.target.getAttribute("data-id"),
+                    new_plan: user_input
+                })
+
+                .then((response) => {
+                    console.log("AXIOS Response Update:", response);
+
+                    const { status, result } = response.data;
+
+                    e.target.parentElement.parentElement.querySelector(
+                        ".item-text"
+                    ).innerHTML = user_input;
+
+                    console.log("Updated plan:", result, user_input);
+                })
+
+                .catch((err) => {
+                    console.log("Updating plan, Error:", err);
+                });
+
+        }
+    }
 });
